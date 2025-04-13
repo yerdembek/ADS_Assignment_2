bd2131f444e1c55747488be352ff9ca1c74c47f9
yerdembek <erdembekb@gmail.com>
2025-04-13 23:57:12 +0500

    init project

--- src/MyMinHeap.java
+++ src/MyMinHeap.java
@@ -1,4 +1,4 @@
-public class MyMinHeap<T> {
+public class MyMinHeap<T extends Comparable<T>> {
     private Object[] heap;
     private int size;
     private static final int INITIAL_CAPACITY = 10;
@@ -8,9 +8,47 @@ public class MyMinHeap<T> {
         size = 0;
     }
 
+    private void bubbleUp(int index) {
+        while (index > 0) {
+            int parentIdx = (index - 1) / 2;
+            T current = (T) heap[index];
+            T parent = (T) heap[parentIdx];
+            if (current.compareTo(parent) < 0) {
+                swap(index, parentIdx);
+                index = parentIdx;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void bubbleDown(int index) {
+        while (index < size) {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int smallest = index;
+
+            if (left < size && ((T) heap[left]).compareTo((T) heap[smallest]) < 0) {
+                smallest = left;
+            }
+            if (right < size && ((T) heap[right]).compareTo((T) heap[smallest]) < 0) {
+                smallest = right;
+            }
+
+            if (smallest != index) {
+                swap(index, smallest);
+                index = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+
+
     public void insert(T item) {
         ensureCapacity();
         heap[size] = item;
+        bubbleUp(size);
         size++;
     }
 
@@ -28,6 +66,7 @@ public class MyMinHeap<T> {
         T min = (T) heap[0];
         heap[0] = heap[size - 1];
         size--;
+        bubbleDown(0);
         return min;
     }
 
