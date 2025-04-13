64965ab0b5458bf07858219cdde7b21bd3b48855
yerdembek <erdembekb@gmail.com>
2025-04-13 22:53:00 +0500

    init project

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        MyList<String> list = new MyLinkedList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        System.out.println("Adding elements...");
+        list.add("One");
+        list.add("Two");
+        list.add("Three");
+
+        System.out.println("Current size: " + list.size());
+
+        System.out.println("Elements:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("- " + list.get(i));
         }
+
+        System.out.println("\nRemoving element at index 1...");
+        list.remove(1);
+
+        System.out.println("After removal:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("- " + list.get(i));
+        }
+
+        System.out.println("\nClearing the list...");
+        list.clear();
+
+        System.out.println("Size after clear: " + list.size());
     }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -6,15 +6,15 @@ public class MyLinkedList<T> implements MyList<T> {
         head = null;
         length = 0;
     }
+
     @Override
     public void add(T item) {
         MyNode<T> newNode = new MyNode<>(item);
-        if(head == null){
+        if (head == null) {
             head = newNode;
-        }
-        else {
+        } else {
             MyNode<T> current = head;
-            while(current.next != null){
+            while (current.next != null) {
                 current = current.next;
             }
             current.next = newNode;
@@ -29,20 +29,47 @@ public class MyLinkedList<T> implements MyList<T> {
 
     @Override
     public void remove(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Index out of range: " + index);
+        }
 
+        if (index == 0) {
+            head = head.next;
+        } else {
+            MyNode<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+        length--;
     }
 
     @Override
     public void clear() {
-
+        head = null;
+        length = 0;
     }
 
     @Override
     public T get(int index) {
+        if (index < 0 || index >= length) {
+            throw new IndexOutOfBoundsException("Index out of range: " + index);
+        }
         MyNode<T> current = head;
-        for(int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++) {
             current = current.next;
         }
         return current.data;
+    }
+
+    private static class MyNode<T> {
+        T data;
+        MyNode<T> next;
+
+        MyNode(T data) {
+            this.data = data;
+            this.next = null;
+        }
     }
 }
